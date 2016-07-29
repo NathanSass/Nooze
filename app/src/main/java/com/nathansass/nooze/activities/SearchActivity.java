a75b4b76a7edcb0f14921ce374c9822cb0dc2a4d
Nathan Sass <nsass711@gmail.com>
2016-07-29 11:51:43 -0700

    Styled the app name

--- app/src/main/java/com/nathansass/nooze/activities/SearchActivity.java
+++ app/src/main/java/com/nathansass/nooze/activities/SearchActivity.java
@@ -54,6 +54,7 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
